5d0fbfe38032facc73f9f165cceac855f6a95a08
bond007 <mihail.ivanov@inbox.ru>
2022-11-27 08:40:47 +0300

    random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 3;
-int NumberB = 5;
+﻿int NumberA = new Random().Next(1,10); // 1 2 3 ... 9
+int NumberB = new Random().Next(1,10); // 1 2 3 ... 9
+Console.WriteLine(NumberA);
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine(result);
